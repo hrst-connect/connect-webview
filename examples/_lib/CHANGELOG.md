3d0ec021cdc3f22fab76d93998cfb557b3b7320f
Raymond Oung <r.oung@hapi-robo.com>
2021-09-27 12:48:13 +0900

    Removes dependencies from robot.js Documents all functions Adds changelog

---
+++ examples/_lib/CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## v1.0.1
+### Maintenance
+- Removes `util.js` dependency from `robot.js` such that it can be used independently
+- Documents all functions
+
+## v1.0.0
+Initial release
--- examples/_lib/robot.js
+++ examples/_lib/robot.js
@@ -1,12 +1,23 @@
-import sleep from './utils'
+/**
+ * Asynchronous sleep function
+ * @param {Number} ms Sleep time [milliseconds] 
+ * @returns Promise that will resolve when sleep is complete
+ */
+ const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 /**
- * Convenience singleton that can be used with Connect or a regular web-browser.
+ * A wrapper around Connect's API that can be used with Connect-Android or a regular web-browser.
  * Used for "simulating" robot's behaviour in a web-browser.
  */
 const robot = (() => {
   const MINIMUM_WAIT = 500 // minimum waiting period between joystick commands [msec]
 
+  /**
+   * Get device ID
+   * @returns Unique device ID
+   */
   const getId = () => {
     let id = 'mockup'
     if (typeof connect !== 'undefined') {
@@ -16,22 +27,21 @@ const robot = (() => {
     return id
   }
 
-  const joystick = (x, y) => {
+  /**
+   * Commands the robot to use it's text-to-speech function.
+   * @param {String} utterance Text that is to be spoken. 
+   */
+   const speak = (utterance) => {
     if (typeof connect !== 'undefined') {
-      connect.joystick(x, y)
+      connect.speak(utterance)
     }
-    console.log(`Joystick: (${x}, ${y})`)
+    console.log(`Speak: ${utterance}`)
   }
 
-  const move = async (x, y, period) => {
-    const iter = Math.round(period / MINIMUM_WAIT)
-    for (let i = 0; i < iter; i++) {
-      joystick(x, y)
-      await sleep(MINIMUM_WAIT)
-      console.log(`Move: (${x}, ${y}) | Iterations: ${i}/${iter}`)
-    }
-  } 
-
+  /**
+   * Turns the robot by a predefined angle.
+   * @param {Number} angle Rotation angle; where positive (+) is counter-clockwise and negative (-) is clockwise [-180, +180] 
+   */
   const turn = (angle) => {
     if (typeof connect !== 'undefined') {
       connect.turn(angle, 1.0)
@@ -39,6 +49,10 @@ const robot = (() => {
     console.log(`Turn by: ${angle}`)
   }
 
+  /**
+   * Tilts the screen of the robot by a predefined angle.
+   * @param {Number} angle Tilt angle; where positive (+) tilts the screen upwards and negative (-) tilts the screen downwards [-25, +45] 
+   */
   const tilt = (angle) => {
     if (typeof connect !== 'undefined') {
       connect.tilt(angle, 1.0, false)
@@ -46,20 +60,47 @@ const robot = (() => {
     console.log(`Tilt by: ${angle}`)
   }
 
-  const gotoPose = (x, y, yaw) => {
+  /**
+   * Sends a single joystick command to the robot. Commands must be continuously sent, otherwise the robot will stop moving after a 500 millisecond timeout.
+   * @param {Number} trans Translational velocity; where -1 is full-speed backwards and +1 is full-speed forwards [-1, +1].
+   * @param {Number} rot Rotational velocity; where -1 is full-speed counter-clockwise and +1 is full-speed clockwise [-1, +1].
+   */
+   const joystick = (trans, rot) => {
     if (typeof connect !== 'undefined') {
-      connect.gotoPose(x, y, yaw)
+      connect.joystick(trans, rot)
     }
-    console.log(`Goto: (${x}, ${y})`)
+    console.log(`Joystick: (${trans}, ${rot})`)
   }
 
-  const gotoLocation = (locationName) => {
-    if (typeof connect !== 'undefined') {
-      connect.gotoLocation(locationName)
+  /**
+   * Continuously sends joystick commands for a predefined period.
+   * @param {Number} trans Translational velocity; where -1 is full-speed backwards and +1 is full-speed forwards [-1, +1].
+   * @param {Number} rot Rotational velocity; where -1 is full-speed counter-clockwise and +1 is full-speed clockwise [-1, +1].
+   * @param {Number} period Period of time in which the robot is commanded to move. 
+   */
+  const move = async (trans, rot, period) => {
+    const iter = Math.round(period / MINIMUM_WAIT)
+    for (let i = 0; i < iter; i++) {
+      joystick(trans, rot)
+      await sleep(MINIMUM_WAIT)
+      console.log(`Move: (${trans}, ${rot}) | Iterations: ${i}/${iter}`)
     }
-    console.log(`Goto: ${locationName}`)
+  } 
+
+  /**
+   * Commands the robot to stop moving.
+   */
+   const stop = () => {
+    if (typeof connect !== 'undefined') {
+      connect.stop()
+    }
+    console.log('Stop')
   }
 
+  /**
+   * Gets the name of all predefined locations.
+   * @returns An array of of location names.
+   */
   const getLocations = () => {
     let locations = []
     if (typeof connect !== 'undefined') {
@@ -69,21 +110,23 @@ const robot = (() => {
     return locations
   }
 
-  const speak = (utterance) => {
+  /**
+   * Sends the robot to a predefined location.
+   * @param {String} locationName Name of the location, as spelled out in the `Locations` list.
+   */
+   const gotoLocation = (locationName) => {
     if (typeof connect !== 'undefined') {
-      connect.speak(utterance)
+      connect.gotoLocation(locationName)
     }
-    console.log(`Speak: ${utterance}`)
+    console.log(`Goto: ${locationName}`)
   }
 
-  const stop = () => {
-    if (typeof connect !== 'undefined') {
-      connect.stop()
-    }
-    console.log('Stop')
-  }
-
-  const getPose = () => {
+  /**
+   * Gets the current pose of the robot, with respect to the Home Base. The coordinate system is defined as follows:
+   * The X-axis points in the direction in front of the Home Base, the Y-axis points left, and Z-axis points up in a right-hand coordinate system.
+   * @returns An object {x, y, yaw} that defines the pose of the robot.
+   */
+   const getPose = () => {
     let pose = { x: 0, y: 0, yaw: 0}
     if (typeof connect !== 'undefined') {
       pose = JSON.parse(connect.getPose())
@@ -93,6 +136,26 @@ const robot = (() => {
     return pose
   }
 
+  /**
+   * Sets the robot's pose, with respect to the Home Base. The coordinate system is defined as follows:
+   * The X-axis points in the direction in front of the Home Base, the Y-axis points left, and Z-axis points up in a right-hand coordinate system.
+   * The Yaw angle is 0 degrees when the robot is sitting on the Home Base.
+   * @param {Number} x Position along the X-axis, with respect to the Home Base.
+   * @param {Number} y Position along the Y-axis, with respect to the Home Base.
+   * @param {Number} yaw Orientiation around the Z-axis, with respect to the Home Base.
+   */
+  const gotoPose = (x, y, yaw) => {
+    if (typeof connect !== 'undefined') {
+      connect.gotoPose(x, y, yaw)
+    }
+    console.log(`Goto: (${x}, ${y})`)
+  }
+
+  /**
+   * Gets the current `goto` status.
+   * @returns A string indicating the current `goto` status. This can be one of the following values:
+   * ['start', 'calculating', 'going', 'complete', 'abort', 'reposing'] 
+   */
   const getGotoStatus = () => {
     let status = null
     if (typeof connect !== 'undefined') {
@@ -104,15 +167,15 @@ const robot = (() => {
 
   return {
     getId,
-    move,
+    speak,
     turn,
     tilt,
-    gotoPose,
-    gotoLocation,
-    getLocations,
-    speak,
+    move,
     stop,
+    getLocations,
+    gotoLocation,
     getPose,
+    gotoPose,
     getGotoStatus,
   }
 })()
