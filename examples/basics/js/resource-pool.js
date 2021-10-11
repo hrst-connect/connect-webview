c0b6a6c2e39b846d8dcd1a5d6d8b98cdac1d2743
Raymond Oung <r.oung@hapi-robo.com>
2021-10-11 15:32:37 +0900

    Fixes typo

--- examples/basics/js/resource-pool.js
+++ examples/basics/js/resource-pool.js
@@ -1,4 +1,4 @@
-const RESOURCE_POOL_ID = 'RESOURCE_POOL_ID'; // insert your resource pool ID here
+const RESOURCE_POOL_ID = 'WYgcE672wZB4cr9KuoAD'; // insert your resource pool ID here
 
 // Resource pool status listener
 function onResourcePoolStatusChange(availableOperators) {
