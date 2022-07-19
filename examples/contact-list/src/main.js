46e7f8135bfeeaa77882635678bd92f52c7de601
Raymond Oung <r.oung@hapi-robo.com>
2022-07-19 15:34:44 +0900

    Removes contact-list

--- examples/contact-list/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Templates and styles
-import 'bootstrap'
-
-import router from './router'
-
-createApp(App).use(router).mount('#app')
